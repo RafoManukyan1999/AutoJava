d5f7d4ba783422ce5229514de6437f19ede61f57
RafoManukyan1999 <rafomanukyan27@gmail.com>
2025-04-05 16:01:16 +0400

    next commit

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,1 +1,21 @@
-import com.sun.org.apache.xpath.internal.objects.XNumber;import org.junit.After;import org.junit.Assert;import org.junit.Before;import org.junit.Test;public class MainClassTest extends MainClass{    @Before    public void testBefore() {System.out.println("Test Start");}    @After    public void testAfter() {System.out.println("Test End");}    @Test    public void testGetMessage(){        String expected = getMessage();        Assert.assertTrue("Word is not included ", expected.contains("Hello"));        System.out.println("Word is included ");    }}
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+    @Before
+    public void testBefore() {System.out.println("Test Start");}
+
+    @After
+    public void testAfter() {System.out.println("Test End");}
+
+    @Test
+    public void testGetMessage(){
+        String expected = getMessage();
+        Assert.assertTrue("Word is not included ", expected.contains("Hello"));
+        System.out.println("Word is included ");
+    }
+}
